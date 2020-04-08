2ada984941905c9bc5639d19045a2064caed54a7
Alessandro Cinquetti <alessandro.cinquetti@gmail.com>
2020-04-08 18:15:27 +0200

    Milestone 2 completo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
 $(document).ready(function() {
 
-    // dichiaro variabili che vanno a prendere gli elementi html che necessito
-    var container = $('.chat');                    // dove deve essere creato il nuovo messaggio
-    var send = $('.right-footer > #send-msg');    // "come" deve essere il messaggio"
+    $('.right-footer > #send-msg').click(sendMsg);            // intercetto il click dell'aeroplanino
 
-    // funzione che invia il messaggio al click dell'aeroplano di carta
-    send.click(
-        function() {
-            var text = $('.right-footer > input').val();
-            container.append('<div class="msg sent">' + text + '</div>');
+
+    $('.right-footer > input').keypress(function(event) {     // intercetto tasto invio sull'input di testo
+        if(event.which == 13) {
+            sendMsg();
         }
-    );
+    });
+
+
+    // FUNZIONE INVIO MESSAGGIO
+    function sendMsg() {
+        var msgContainer = $('.chat');
+        var text = $('.right-footer > input').val();                         //salvo testo scritto da utente
+
+        msgContainer.append('<div class="msg sent">' + text + '</div>');     // inietto testo scritto
+
+        $('.right-footer > input').val('');                         // svuoto input di testo
+
+        setTimeout(recievedMsg, 1000);                              // Imposto timeout function per risposta automatica
+    }
+
+    // FUNZIONE RICEZIONE MESSAGGIO
+    function recievedMsg() {
+        var msgContainer = $('.chat');
+        msgContainer.append('<div class="msg recieved">' + 'ok!' + '</div>');   // inietto testo "statico"
+    }
+
+
+
+    // FUNZIONE RICERCA CONTATTO
+    $('#search-contact').keyup(function(event) {
+        var searchedLetter = $(this).val().toLowerCase();                        //salvo carattere digitato da utente
+
+        $('.contact').each(function() {                                          //passo in rassegna contatti
+            var userName = $(this).find('.contact-name').text().toLowerCase();   //salvo nome contatti
+
+            if(userName.includes(searchedLetter)) {                              //condizioni per lo show
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        })
+    })
+
 });
